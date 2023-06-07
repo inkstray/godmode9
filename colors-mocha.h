c8789056012ae581c29fa3888261eb7ea9bf7e87
Tryzen-0 <80412260+Tryzen-0@users.noreply.github.com>
2023-06-07 22:24:24 +0200

    update colors to be in a terrible order

--- colors-mocha.h
+++ colors-mocha.h
@@ -6,64 +6,65 @@
 	((u32)(b) >> 3))
 
 // a base set of colors below
-#define COLOR_BLACK         RGB(0x00, 0x00, 0x00)
-#define COLOR_WHITE         RGB(0xFF, 0xFF, 0xFF)
-#define COLOR_GREY          RGB(0x80, 0x80, 0x80)
+// everything marked with a ? im unsure whether to use a specific color
+#define COLOR_BLACK         RGB(0x18, 0x18, 0x25) // Mantle
+#define COLOR_WHITE         RGB(0xCD, 0xD6, 0xF4) // Text 
+#define COLOR_GREY          RGB(0x58, 0x5B, 0x70) // Surface2
 
-#define COLOR_RED           RGB(0xF3, 0x8B, 0xA8)
-#define COLOR_GREEN         RGB(0xA6, 0xE3, 0xA1)
-#define COLOR_BLUE          RGB(0x89, 0xB4, 0xFA)
-#define COLOR_YELLOW        RGB(0xF9, 0xE2, 0xAF)
-#define COLOR_CYAN          RGB(0x94, 0xE2, 0xD5)
-#define COLOR_ORANGE        RGB(0xFA, 0xB3, 0x87)
+#define COLOR_RED           RGB(0xF3, 0x8B, 0xA8) // Red
+#define COLOR_GREEN         RGB(0xA6, 0xE3, 0xA1) // Green
+#define COLOR_BLUE          RGB(0x89, 0xB4, 0xFA) // Blue
+#define COLOR_YELLOW        RGB(0xF9, 0xE2, 0xAF) // Yellow
+#define COLOR_CYAN          RGB(0x94, 0xE2, 0xD5) // Teal
+#define COLOR_ORANGE        RGB(0xFA, 0xB3, 0x87) // Peach
 
-#define COLOR_BRIGHTRED     RGB(0xF3, 0x8B, 0xA8)
-#define COLOR_DARKRED       RGB(0xEB, 0x8B, 0xA8)
-#define COLOR_BRIGHTYELLOW  RGB(0xF9, 0xE2, 0xAF)
-#define COLOR_BRIGHTGREEN   RGB(0xA6, 0xE3, 0xA1)
-#define COLOR_BRIGHTBLUE    RGB(0x74, 0xC7, 0xEC)
-// #bedcd4 we are here
-#define COLOR_TINTEDBLUE    RGB(0x60, 0x60, 0x80)
-#define COLOR_TINTEDYELLOW  RGB(0xD0, 0xD0, 0x60)
-#define COLOR_TINTEDGREEN   RGB(0x70, 0x80, 0x70)
-#define COLOR_LIGHTGREY     RGB(0xB0, 0xB0, 0xB0)
-#define COLOR_LIGHTERGREY   RGB(0xD0, 0xD0, 0xD0)
-#define COLOR_DARKGREY      RGB(0x50, 0x50, 0x50)
-#define COLOR_DARKESTGREY   RGB(0x20, 0x20, 0x20)
-#define COLOR_SUPERFUCHSIA  RGB(0xFF, 0x00, 0xEF)
+#define COLOR_BRIGHTRED     RGB(0xF3, 0x8B, 0xA8) // Red
+#define COLOR_DARKRED       RGB(0xEB, 0x8B, 0xA8) // Maroon
+#define COLOR_BRIGHTYELLOW  RGB(0xF9, 0xE2, 0xAF) // Yellow
+#define COLOR_BRIGHTGREEN   RGB(0xA6, 0xE3, 0xA1) // Green
+#define COLOR_BRIGHTBLUE    RGB(0x89, 0xB4, 0xFA) // Blue
+
+#define COLOR_TINTEDBLUE    RGB(0xB4, 0xBE, 0xFE) // Lavender
+#define COLOR_TINTEDYELLOW  RGB(0xF9, 0xE2, 0xAF) // Yellow
+#define COLOR_TINTEDGREEN   RGB(0xA6, 0xE3, 0xA1) // Green
+#define COLOR_LIGHTGREY     RGB(0x6C, 0x70, 0x86) // overlay0
+#define COLOR_LIGHTERGREY   RGB(0x93, 0x99, 0xB2) // overlay2
+#define COLOR_DARKGREY      RGB(0x45, 0x47, 0x5A) // surface1
+#define COLOR_DARKESTGREY   RGB(0x31, 0x32, 0x44) // Surface0
+#define COLOR_SUPERFUCHSIA  RGB(0xCB, 0xA6, 0xF7) // Mauve
 
 // standard colors - used everywhere
 #ifndef COLOR_STD_BG
-#define COLOR_STD_BG        RGB(0x1E, 0x1E, 0x2E)
+#define COLOR_STD_BG        RGB(0x1E, 0x1E, 0x2E) // Base
 #endif
 #ifndef COLOR_STD_FONT
-#define COLOR_STD_FONT      RGB(0xCD, 0xD6, 0xF4)
+#define COLOR_STD_FONT      RGB(0xCD, 0xD6, 0xF4) // Text
 #endif
 
 // colors for GodMode9 file browser
-#define COLOR_SIDE_BAR      COLOR_DARKGREY
-#define COLOR_MARKED        COLOR_TINTEDYELLOW
-#define COLOR_FILE          COLOR_TINTEDGREEN
-#define COLOR_DIR           COLOR_TINTEDBLUE
-#define COLOR_ROOT          COLOR_GREY
+#define COLOR_SIDE_BAR      RGB(0x11, 0x11, 0x1b) // Crust
+#define COLOR_MARKED        COLOR_TINTEDYELLOW // yellow
+#define COLOR_FILE          COLOR_TINTEDGREEN // green
+#define COLOR_DIR           RGB(0x89, 0xB4, 0xFA) // Blue
+#define COLOR_ROOT          RGB(0xA6, 0xAD, 0xC8) // Subtext0
 #define COLOR_ENTRY(e)  (((e)->marked) ? COLOR_MARKED : ((e)->type == T_DIR) ? COLOR_DIR : ((e)->type == T_FILE) ? COLOR_FILE : ((e)->type == T_ROOT) ?  COLOR_ROOT : COLOR_GREY)
 
 // hex viewer colors
-#define COLOR_HVOFFS        RGB(0x40, 0x60, 0x50)
-#define COLOR_HVOFFSI       COLOR_DARKESTGREY
-#define COLOR_HVASCII       RGB(0x40, 0x80, 0x50)
-#define COLOR_HVHEX(i)      ((i % 2) ? RGB(0x30, 0x90, 0x30) : RGB(0x30, 0x80, 0x30))
+#define COLOR_HVOFFS        RGB(0xA6, 0xAD, 0xC8) // subtext0
+#define COLOR_HVOFFSI       RGB(0x7E, 0x84, 0x9C) // Overlay1
+#define COLOR_HVASCII       RGB(0xBA, 0xC2, 0xDE) // subtext1
+#define COLOR_HVHEX(i)      ((i % 2) ? RGB(0xCD, 0xD6, 0xF4) : RGB(0xBA, 0xC2, 0xDE))
 
 // text viewer / script viewer colors
 #define COLOR_TVOFFS        RGB(0x40, 0x40, 0x40)
 #define COLOR_TVOFFSL       RGB(0x30, 0x30, 0x30)
 #define COLOR_TVTEXT        RGB(0xA0, 0xA0, 0xA0)
-#define COLOR_TVRUN         RGB(0xC0, 0x00, 0x00)
+#define COLOR_TVRUN         RGB(0xF3, 0x8B, 0xA8) // Red
 #define COLOR_TVCMT         RGB(0x60, 0x60, 0x70)
 #define COLOR_TVCMD         RGB(0xA0, 0xA0, 0xA0)
 
 // battery symbol colors
-#define COLOR_BATTERY_CHARGING  RGB(0x3D, 0xB7, 0xE4)
-#define COLOR_BATTERY_FULL      RGB(0x0F, 0xB0, 0x1B)
-#define COLOR_BATTERY_MEDIUM    RGB(0xFF, 0x88, 0x49)
-#define COLOR_BATTERY_LOW       RGB(0xB4, 0x00, 0x00)
+#define COLOR_BATTERY_CHARGING  RGB(0x74, 0xC7, 0xEC) // Sapphire
+#define COLOR_BATTERY_FULL      RGB(0xA6, 0xE3, 0xA1) // Green
+#define COLOR_BATTERY_MEDIUM    RGB(0xFA, 0xB3, 0x87) // Peach
+#define COLOR_BATTERY_LOW       RGB(0xEB, 0xA0, 0xAC) // Maroon
